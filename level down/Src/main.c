6cd322e7324dc5155fbeb84ca2695a6a04b97137
grimeshef <alexmaltsevvik@gmail.com>
2019-10-11 15:58:33 +0300

    Start

---
+++ level down/Src/main.c
@@ -0,0 +1,493 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_cdc_if.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim1;
+TIM_HandleTypeDef htim4;
+
+/* USER CODE BEGIN PV */
+uint8_t data_rx[8];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM1_Init(void);
+static void MX_TIM4_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+int st_min = 1068 + 1;
+int st_max = 1775 - 1;
+int st_mid = 1415;
+int th_min = 1232 + 1;
+int th_max = 1735 - 1;
+int th_mid = 1512; //rev minimum 29%; fwd minimum 4%
+int waiting = 0;
+
+
+void fwd(float persent, double plavn)
+{
+    int znach = (th_mid + (th_max - th_mid) * persent);
+    if (plavn == 0)
+    {
+        TIM4->CCR1 = znach;
+    }
+    else
+    {
+        while (TIM4->CCR1 < znach)
+        {
+            TIM4->CCR1 = TIM4->CCR1 + 1;
+            HAL_Delay(plavn);
+            waiting = waiting + plavn;
+        }
+    }
+}
+
+void rev(float persent, double plavn)
+{
+    int znach = (th_mid - (th_mid - th_min) * persent);
+    if (plavn == 0)
+    {
+        TIM4->CCR1 = znach;
+    }
+    else
+    {
+        while (TIM4->CCR1 > znach)
+        {
+            TIM4->CCR1 = TIM4->CCR1 - 1;
+            HAL_Delay(plavn);
+            waiting = waiting + plavn;
+        }
+    }
+}
+
+void left(int persent, double plavn)
+{
+    int znach = (st_mid - (st_mid - st_min) * persent);
+    TIM4->CCR2 = znach;
+    if (plavn == 0)
+    {
+        TIM4->CCR2 = znach;
+    }
+    else
+    {
+        while (TIM4->CCR2 > znach)
+        {
+            TIM4->CCR2 = TIM4->CCR2 - 1;
+            HAL_Delay(plavn);
+            waiting = waiting + plavn;
+        }
+    }
+}
+
+
+void right(int persent, double plavn) {
+    int znach = (st_mid + (st_max - st_mid) * persent);
+    TIM4->CCR2 = znach;
+    if (plavn == 0) {
+        TIM4->CCR2 = znach;
+    }
+    else {
+        while (TIM4->CCR2 < znach) {
+            TIM4->CCR2 = TIM4->CCR2 + 1;
+            HAL_Delay(plavn);
+            waiting = waiting + plavn;
+        }
+    }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+  
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USB_DEVICE_Init();
+  MX_TIM1_Init();
+  MX_TIM4_Init();
+  /* USER CODE BEGIN 2 */
+       float _pwm_turn=0;
+       float _pwm_run=0;
+       uint8_t data_rx_turn[4];
+       uint8_t data_rx_run[4];
+       HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
+       HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);
+       HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_2);
+
+    union {
+        //float turn;
+        float run;
+        unsigned char bytes_run[4];
+    } thing;
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) {
+         for (int i = 0;i<4;i++){
+             data_rx_turn[i]=data_rx[i];
+         }
+         for (int i = 4;i<8;i++){
+             data_rx_run[i-4]=data_rx[i];
+         }
+         memcpy(&_pwm_turn, &data_rx_turn, sizeof(float));
+         memcpy(&_pwm_run, &data_rx_run, sizeof(float));
+         //thing.turn=_pwm_turn;
+         thing.run=_pwm_run;
+         CDC_Transmit_FS(thing.bytes_run, 4);
+
+        if (_pwm_run>=0) {
+            fwd(_pwm_run, 0);
+            HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_14);
+        }
+        else if (_pwm_run<0){
+            fwd(0,0);
+            _pwm_run=-1*_pwm_run;
+            rev(_pwm_run,0);
+            HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_13);
+        }
+        
+        if (_pwm_turn>=0){
+            right(_pwm_turn,0);
+        }
+        else if (_pwm_turn<0) {
+            _pwm_turn=-1*_pwm_turn;
+            left(_pwm_turn,0);
+        }
+        
+
+      /*  HAL_Delay(1000);
+        fwd(5,0);
+        HAL_Delay(1000);
+        fwd(0,0);
+        HAL_Delay(1000);
+        rev(40,0);
+        HAL_Delay(1000);
+        fwd(0,0);
+        HAL_Delay(1000);
+        rev(30,0);
+        HAL_Delay(1000);
+        fwd(0,0);
+        HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_13);*/
+
+     }
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage 
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB busses clocks 
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM1_Init(void)
+{
+
+  /* USER CODE BEGIN TIM1_Init 0 */
+
+  /* USER CODE END TIM1_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM1_Init 1 */
+
+  /* USER CODE END TIM1_Init 1 */
+  htim1.Instance = TIM1;
+  htim1.Init.Prescaler = 0;
+  htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim1.Init.Period = 65535;
+  htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim1.Init.RepetitionCounter = 0;
+  htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim1, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM1_Init 2 */
+
+  /* USER CODE END TIM1_Init 2 */
+  HAL_TIM_MspPostInit(&htim1);
+
+}
+
+/**
+  * @brief TIM4 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM4_Init(void)
+{
+
+  /* USER CODE BEGIN TIM4_Init 0 */
+
+  /* USER CODE END TIM4_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM4_Init 1 */
+
+  /* USER CODE END TIM4_Init 1 */
+  htim4.Instance = TIM4;
+  htim4.Init.Prescaler = 83;
+  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim4.Init.Period = 19999;
+  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM4_Init 2 */
+
+  /* USER CODE END TIM4_Init 2 */
+  HAL_TIM_MspPostInit(&htim4);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PD14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ level up/scratch_1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# license removed for brevity
+import rospy
+import serial
+from builtins import bytearray, print, type, range, int
+from sensor_msgs.msg import Joy, struct
+
+serialData = serial.Serial('/dev/ttyACM0', 115200,
+                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+serialString = ""  # Used to hold data coming over UART
+
+
+# serialData.write(b"\x01\r\n")
+# while serialData.readable():
+
+def callback(data):
+    global joy_axes
+    joy_axes = []
+    byte_joy_axes = bytearray()
+    joy_axes.append(data.axes[0])
+    joy_axes.append(data.axes[1])
+    rospy.loginfo(data.axes)
+    print(joy_axes)
+    # while serialData.readable():
+    byte_joy_axes += struct.pack('f', joy_axes[0])
+    byte_joy_axes += struct.pack('f', joy_axes[1])
+    print(byte_joy_axes)
+
+    serialData.write(byte_joy_axes)
+
+    reciver = []
+    reciver.append(serialData.read(4))
+    print(*reciver)
+    # print(struct.unpack('f',reciver))
+    # print(int.from_bytes(serialData.read(),byteorder='little'))
+
+
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber('joy', Joy, callback)
+    rospy.spin()
+
+
+while not rospy.is_shutdown():
+    if __name__ == "__main__":
+        listener()
+
+# b+=struct.pack('f',i)
+# b+=struct.pack('f',190.5)
+# print(b)
+# serialData.write(b)
+# print(int.from_bytes(serialData.read(), byteorder='little'))
+# print(serialData.read())
+# print(i)
+# b.clear()
+# print(serialData.read().decode('Ascii'))
+# serialString = serialData.readline()
+# print(serialString.decode('Ascii'))
