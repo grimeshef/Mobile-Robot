5dea283557156b44ed23363c8d9eeac5d8be6df7
grimeshef <alexmaltsevvik@gmail.com>
2019-10-11 16:14:17 +0300

    change level up

--- level up/scratch_1.py
+++ level up/scratch_1.py
@@ -8,21 +8,13 @@ from sensor_msgs.msg import Joy, struct
 serialData = serial.Serial('/dev/ttyACM0', 115200,
                            bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
 
-serialString = ""  # Used to hold data coming over UART
-
-
-# serialData.write(b"\x01\r\n")
-# while serialData.readable():
-
 def callback(data):
-    global joy_axes
     joy_axes = []
     byte_joy_axes = bytearray()
     joy_axes.append(data.axes[0])
     joy_axes.append(data.axes[1])
     rospy.loginfo(data.axes)
     print(joy_axes)
-    # while serialData.readable():
     byte_joy_axes += struct.pack('f', joy_axes[0])
     byte_joy_axes += struct.pack('f', joy_axes[1])
     print(byte_joy_axes)
@@ -32,9 +24,6 @@ def callback(data):
     reciver = []
     reciver.append(serialData.read(4))
     print(*reciver)
-    # print(struct.unpack('f',reciver))
-    # print(int.from_bytes(serialData.read(),byteorder='little'))
-
 
 def listener():
     rospy.init_node('listener', anonymous=True)
@@ -46,6 +35,7 @@ while not rospy.is_shutdown():
     if __name__ == "__main__":
         listener()
 
+# while serialData.readable():
 # b+=struct.pack('f',i)
 # b+=struct.pack('f',190.5)
 # print(b)
